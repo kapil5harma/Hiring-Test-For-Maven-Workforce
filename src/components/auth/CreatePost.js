33e5a0c274183cab4084ed9927f63df52ea6f6e0
Kapil Sharma <kapilsharmajmps@gmail.com>
2018-06-24 22:35:50 +0530

    Basic form Validation done

--- src/components/auth/CreatePost.js
+++ src/components/auth/CreatePost.js
@@ -17,7 +17,12 @@ class CreatePost extends Component {
       phone: '',
       website: '',
       redirectToNewPage: false,
-      errors: {},
+      errors: {
+        name: false,
+        email: false,
+        phone: false,
+        website: false
+      },
       submitteData: {}
     };
 
@@ -28,15 +33,10 @@ class CreatePost extends Component {
   componentDidMount = () => {};
 
   onChange(e) {
-    console.log('e: ', e);
-    console.log('onChange');
     this.setState({ [e.target.name]: e.target.value });
-    console.log('e.target.name: ', e.target.name);
-    console.log('e.target.value: ', e.target.value);
   }
 
   onSubmit(e) {
-    console.log('onSubmit');
     e.preventDefault();
     const postedData = {
       name: this.state.name,
@@ -45,19 +45,98 @@ class CreatePost extends Component {
       website: this.state.website
     };
 
-    //redirect to new page
-    this.setState({ redirectToNewPage: true, submitteData: postedData });
+    this.handleValidation();
+    if (this.handleValidation()) {
+      // Show submitted data
+      this.setState({ redirectToNewPage: true, submitteData: postedData });
+    }
+  }
+
+  handleValidation() {
+    let fields = {
+      name: this.state.name,
+      email: this.state.email,
+      phone: this.state.phone,
+      website: this.state.website
+    };
+    let errors = this.state.errors;
+
+    //Name
+    if (!fields.name) {
+      errors.name = true;
+    } else if (typeof fields.name !== 'undefined') {
+      if (!fields.name.match(/^([a-z']+(-| )?)+$/i)) {
+        errors.name = true;
+      } else {
+        errors.name = false;
+      }
+    }
+
+    //Email
+    if (!fields.email) {
+      errors.email = true;
+    } else if (typeof fields.email !== 'undefined') {
+      let lastAtPos = fields.email.lastIndexOf('@');
+      let lastDotPos = fields.email.lastIndexOf('.');
+
+      if (
+        !(
+          lastAtPos < lastDotPos &&
+          lastAtPos > 0 &&
+          fields.email.indexOf('@@') === -1 &&
+          lastDotPos > 2 &&
+          fields.email.length - lastDotPos > 2
+        )
+      ) {
+        errors.email = true;
+      } else {
+        errors.email = false;
+      }
+    }
+
+    //Phone Number
+    if (!fields.phone) {
+      errors.phone = true;
+    } else if (typeof fields.phone !== 'undefined') {
+      if (!fields.phone.match(/^[789]\d{9}$/)) {
+        errors.phone = true;
+      } else {
+        errors.phone = false;
+      }
+    }
+
+    //Website
+    if (!fields.website) {
+      errors.website = true;
+    }
+    if (typeof fields.website !== 'undefined') {
+      if (
+        !fields.website.match(
+          /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/
+        )
+      ) {
+        errors.website = true;
+      } else {
+        errors.website = false;
+      }
+    }
+
+    this.setState({ errors: errors });
+    if (
+      this.state.errors.name === false &&
+      this.state.errors.email === false &&
+      this.state.errors.phone === false &&
+      this.state.errors.website === false
+    ) {
+      return true;
+    } else return false;
   }
 
   render() {
     if (this.state.redirectToNewPage) {
       return (
         <div>
-          <Grid
-            textAlign="center"
-            style={{ height: '100%', marginTop: '5%' }}
-            // verticalAlign="middle"
-          >
+          <Grid textAlign="center" style={{ height: '100%', marginTop: '5%' }}>
             <Grid.Column style={{ maxWidth: 450 }}>
               <Container textAlign="left">
                 <Header style={{ marginBottom: '10%' }} as="h2">
@@ -109,11 +188,11 @@ class CreatePost extends Component {
                   name="name"
                   icon="user"
                   iconPosition="left"
-                  placeholder="Name"
+                  placeholder="Full Name"
                   autoComplete="name"
                   value={this.state.name}
                   onChange={this.onChange}
-                  // error={errors.name}
+                  error={this.state.errors.name}
                 />
                 <Form.Input
                   fluid
@@ -124,30 +203,30 @@ class CreatePost extends Component {
                   autoComplete="email"
                   value={this.state.email}
                   onChange={this.onChange}
-                  // error={errors.name}
+                  error={this.state.errors.email}
                 />
                 <Form.Input
                   fluid
                   name="phone"
                   icon="phone"
                   iconPosition="left"
-                  placeholder="Phone"
+                  placeholder="10 digit mobile number"
                   type="phone"
                   autoComplete="tel"
                   value={this.state.phone}
                   onChange={this.onChange}
-                  // error={errors.name}
+                  error={this.state.errors.phone}
                 />
                 <Form.Input
                   fluid
                   name="website"
                   icon="laptop"
                   iconPosition="left"
-                  placeholder="Website"
+                  placeholder="Website (https://www.kapil5harma.com)"
                   type="website"
                   value={this.state.website}
                   onChange={this.onChange}
-                  // error={errors.name}
+                  error={this.state.errors.website}
                 />
 
                 <Button type="submit" color="teal" fluid size="large">
